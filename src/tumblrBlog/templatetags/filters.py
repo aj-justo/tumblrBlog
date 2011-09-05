189e5c0ca2d4eda0e44e366ffa536e348269563b
Angel Justo <aj@ajmacbook.local>
2011-09-05 12:03:09 +0200

    Necessary corrections in templates and view after the refactoring of the model and the methods to access the posts. No need for get_key filter now. Everything is working fine with the views and templates now: home page shows latest posts, single page shows post correctly.

--- src/tumblrBlog/templatetags/filters.py
+++ src/tumblrBlog/templatetags/filters.py
@@ -8,13 +8,16 @@ import datetime
 from django import template
 register = template.Library()
 
-def getkey(value, arg):
-    return value[arg]
+#def getkey(value, arg):
+#    return value[arg]
 
 def time_to_date(value):
-    return datetime.datetime.fromtimestamp(value)
+    try:
+        return datetime.datetime.fromtimestamp(value)
+    except TypeError:
+        return value
 
-register.filter('getkey', getkey)
+#register.filter('getkey', getkey)
 register.filter('time_to_date', time_to_date)
 
 
--- src/tumblrBlog/views.py
+++ src/tumblrBlog/views.py
@@ -4,21 +4,20 @@ Created on Jul 8, 2011
 @author: ajweb.eu
 '''
 from django.template import Context, loader
-from tumblrBlog.models import Post
 from tumblrBlog import tumblrPosts
 from django.http import HttpResponse
 
 
 def index(request):
-    posts = Post.latest()
-   # posts = tumblrPosts.get()
+    posts = tumblrPosts.getLatestPosts()
+    # posts = tumblrPosts.getTumblrPosts()
     template = loader.get_template('tumblrBlog/index.html')
     context = Context({'latest_posts':posts,})
     
     return HttpResponse(template.render(context))
 
 def single(request, post_id):
-    post = Post.objects.get(post_id=post_id)
+    post = tumblrPosts.getPost(id=int(post_id))
     template = loader.get_template('tumblrBlog/single.html')
     context = Context({'post':post,})
     return HttpResponse(template.render(context))        
