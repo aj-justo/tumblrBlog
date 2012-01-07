ecfdd59873d574df4e822ec8f4eedfa4e01a366a
AJ <info@ajweb.eu>
2012-01-07 11:17:42 +0100

    refactoring of methods used for cache checks and app_label added for models and minor modifications

--- src/tumblrBlog/models.py
+++ src/tumblrBlog/models.py
@@ -1,12 +1,12 @@
-'''
+"""
 Created on Jul 7, 2011
 
 @author: aj
-'''
+"""
 from django.db import models
 from util.tumblr import Api, TumblrError 
 from tumblrBlog import settings
-import datetime, hashlib, time, inspect, sys
+import datetime, time
 import django.core.exceptions as dexceptions
 
 class Post(models.Model):    
@@ -18,6 +18,9 @@ class Post(models.Model):
     tumblr_url = models.URLField('Permalink')
     type = models.CharField('Type', max_length=255)
     format = models.CharField(max_length=255)
+
+    class Meta:
+        app_label = 'tumblrBlog'
           
         
     @classmethod
@@ -39,6 +42,9 @@ class Tag(models.Model):
     tag_id = models.IntegerField()
     post_id = models.ForeignKey(Post)
     tag = models.CharField(max_length=255)
+
+    class Meta:
+        app_label = 'tumblrBlog'
     
     
 #class Settings(models.Model):
@@ -53,139 +59,129 @@ class Tag(models.Model):
 # not a model but linking the tumblr wrapper with the local cache models
 
 class tumblrPosts(object):
-    '''
+    """
         methods to retrieve posts from cache and from tumblr,
         sync cache, set settings, etc
         todo: refactor to include only the intermediation between
         model and tumblr. Settings should go in settings model in future
-    '''
+    """
     
     overrideTTL = None
     
     @classmethod
-    def getTumblrPosts(self): 
-    # if optionsQuery: options = __queryToOptions(optionsQuery)
+    def getTumblrPosts(cls):
+        # if optionsQuery: options = __queryToOptions(optionsQuery)
         # but first without options
-        t_api = Api(self.getTumblrUser())
+        t_api = Api(cls.getTumblrUser())
         return t_api.read()        
     
     @classmethod
-    def getTTL(self):
-        return self.overrideTTL or  settings.TUMBLRBLOG_CACHE_TIME_TO_LIVE
+    def getTTL(cls):
+        return cls.overrideTTL or  settings.TUMBLRBLOG_CACHE_TIME_TO_LIVE
     
     @classmethod
-    def getTumblrUser(self):
+    def getTumblrUser(cls):
         return settings.TUMBLRBLOG_TUMBLR_USER
     
     @classmethod
-    def setOverrideTTL(self,ttl):
-        '''
+    def setOverrideTTL(cls,ttl):
+        """
         set the time to live for the cache of the posts,
         in minutes, overriding the global setting
-        '''
-        self.overrideTTL = ttl
+        """
+        cls.overrideTTL = ttl
         
     # todo: setter and getter settings
-    
     # todo: check the time of last checking, when that is implemented
     @classmethod
-    def refreshCacheNeeded(self, latest_in_cache_time=None):
-        if latest_in_cache_time == None:
+    def refreshCacheNeeded(cls, latest_in_cache_time=None):
+        if latest_in_cache_time is None:
             try:
                 latest_in_cache_time = Post.objects.all().order_by('date')[:1]['date']
-            except:
+            except Exception:
                 return True
-        cache_time = datetime.timedelta(minutes=self.getTTL())
-    
-        if cache_time == 0: return self.checkCacheSync() # we want to refresh everytime
+        cache_time = datetime.timedelta(minutes=cls.getTTL())
+        if not cache_time: return cls.checkCacheSync() # we want to refresh everytime
         # if the TTL has passed, refresh
         # we use last entry on cache while db settings with last_refresh is not implemented
-    
-        if datetime.datetime.fromtimestamp(latest_in_cache_time) <= (datetime.datetime.now() - cache_time): 
-            return self.checkCacheSync()
-        
-        return False   
-    
+        if datetime.datetime.fromtimestamp(latest_in_cache_time) \
+                <= (datetime.datetime.now() - cache_time):
+            return cls.checkCacheSync()
+        return False
+
     @classmethod
-    def checkCacheSync(self, localItems=None, remoteItems=None):
-        '''
+    def checkCacheSync(cls, localItems=None, remoteItems=None):
+        """
             check if there are tumblr posts that are not in cache
             Args: optionally, the two lists of posts to be compared,
                     they are otherwise taken from tumblr api and local cache
             Returns: True if there are posts in Tumblr that are not in cache, False otherwise
-        '''
-        if localItems==None:
-            localItems = self.localPosts()   
-        if remoteItems==None:
-            remoteItems = self.remotePosts()
-            
-        return self._comparePostsLists(localItems, remoteItems, ['regular_body','regular-body'])   
-            
+        """
+        if localItems is None:
+            localItems = cls.localPosts()
+        if remoteItems is None:
+            remoteItems = cls.remotePosts()
+        return cls._arePostsListsEqual(localItems, remoteItems, ['regular_body','regular-body'])
+
     @classmethod
-    def _comparePostsLists(self,list1, list2, fields):   
+    def _arePostsListsEqual(cls, list1, list2, fields):
         if len(list1) != len(list2): return True
-        i = 0
-        for post in list1:     
-            try:                 
-                for field in fields:
-                    try: list1hash = hashlib.sha1(post[field])     
-                    except KeyError: continue          
-            except (TypeError, KeyError):
-                list1hash = hashlib.sha1( getattr(post, field) )
-            try:
-                for field in fields:
-                    try: list2hash = hashlib.sha1(list2[i][field]) 
-                    except KeyError: continue
-            except (TypeError, KeyError):
-                list2hash = hashlib.sha1( getattr(list2[i], field) )
-            
-    #        for post in list1: print vars(post)
-    #        for post in list2: print vars(post)
-            
-            if list1hash.hexdigest() != list2hash.hexdigest():
-                return True
-            i = i + 1
+        if cls._getListOfPostsFields(list1, fields) != \
+            cls._getListOfPostsFields(list2, fields): return True
         return False
+
+    @classmethod
+    def _getListOfPostsFields(cls, postsList, fields):
+        filteredList = []
+        for post in postsList:
+            for field in fields:
+                try:
+                    try: filteredList = post[field]
+                    except KeyError: continue
+                except (TypeError, KeyError):
+                    try: filteredList = getattr(post, field)
+                    except AttributeError: continue
+        return filteredList
         
     @classmethod    
-    def syncWithTumblr(self):
-            try:
-                if self.refreshCacheNeeded():
-                    for post in self.getTumblrPosts():
-                        #todo: check not already in cache - or will this failed if an existing id is passed?
-                        Post.TumblrPostToCache(post)
-                return Post.objects.all()
-            except Exception: return Post.objects.all()
+    def syncWithTumblr(cls):
+        try:
+            if cls.refreshCacheNeeded():
+                for post in cls.getTumblrPosts():
+                    #todo: check not already in cache - or will this failed if an existing id is passed?
+                    Post.TumblrPostToCache(post)
+            return Post.objects.all()
+        except Exception: return Post.objects.all()
     
     @classmethod     
-    def localPosts(self):
+    def localPosts(cls):
         posts = []
         [ posts.append(post) for post in Post.objects.all().order_by('-date') ]
         return posts
     
     @classmethod
-    def remotePosts(self):       
+    def remotePosts(cls):
         posts = []
         try:
-            [ posts.append(post) for post in Api(self.getTumblrUser()).read() ]
+            [ posts.append(post) for post in Api(cls.getTumblrUser()).read() ]
         except TumblrError:
             return posts
         return posts
     
     @classmethod
-    def getLatestPosts(self, limit=settings.TUMBLRBLOG_MAX_POSTS_HOME_PAGE):
-        self.syncWithTumblr()
+    def getLatestPosts(cls, limit=settings.TUMBLRBLOG_MAX_POSTS_HOME_PAGE):
+        cls.syncWithTumblr()
         return Post.objects.all().order_by('-date')[:limit]
     
     @classmethod
-    def getPost(self, id):
-        self.syncWithTumblr()
+    def getPost(cls, id):
+        cls.syncWithTumblr()
         try: return Post.objects.get(post_id=id)
         except dexceptions.ObjectDoesNotExist: return False
     
     @classmethod    
-    def getPosts(self, date=datetime.datetime.now(), daysback=10, limit=settings.TUMBLRBLOG_MAX_POSTS_HOME_PAGE):
-        self.syncWithTumblr()
+    def getPosts(cls, date=datetime.datetime.now(), daysback=10, limit=settings.TUMBLRBLOG_MAX_POSTS_HOME_PAGE):
+        cls.syncWithTumblr()
         dateback = date - datetime.timedelta(days=daysback)
         dateback = time.mktime(dateback.timetuple())
         try: 
@@ -193,6 +189,6 @@ class tumblrPosts(object):
         except dexceptions.ObjectDoesNotExist: return False
     
     @classmethod        
-    def cleanCache(self):
-        Post.objects.all().delete()    
+    def cleanCache(cls):
+        Post.objects.all().delete()
         
